a179f96259c387085339805424e20cf16adb2c86
Sweekruthi Raghunathan <raghus@uw.edu>
2021-05-20 08:17:21 -0700

    add react geolocate

--- client/src/Looking.jsx
+++ client/src/Looking.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Button } from '@blueprintjs/core';
 import { Link, useLocation } from 'react-router-dom';
+/* import { geolocated } from "react-geolocated"; */
 
 function Looking(props) {
   const location = useLocation();
   const { clueNumber } = location.state;
 
+  /*  return !this.props.isGeolocationAvailable ? (
+        <div>Your browser does not support Geolocation</div>
+    ) : !this.props.isGeolocationEnabled ? (
+        <div>Geolocation is not enabled</div>
+    ) : this.props.coords ? ( */
   return (
     <div className="app-width">
       <p>Find the trash cans in the HUB with big screens behind them.</p>
@@ -22,6 +28,9 @@ function Looking(props) {
       </div>
     </div>
   );
+  /* ) : (
+            <div>Getting the location data&hellip; </div>
+    ); */
 }
 
 export default Looking;
