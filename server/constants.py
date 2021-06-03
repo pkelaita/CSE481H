1f4816ebcd0f8129a33e24ca30b8c58cad1e9db9
Pierce Kelaita <pierce@Pierces-MacBook-Pro.local>
2021-06-03 00:09:51 -0700

    added random choice for personalization

--- server/constants.py
+++ server/constants.py
@@ -10,34 +10,28 @@ HENRY_AG = 'Henry Art Gallery'
 LAW_LIB = 'Gallagher Law Library'
 
 # Topics
-WILDLIFE = 'Wildlife'
 CARBON = 'Carbon emissions'
 COMPOST = 'Composting'
 MICRO = 'Microplastics'
 GARDENING = 'Gardening'
 CLIMATE = 'Climate change'
 ENVIRO = 'Environmental effects of art'
-REDUCING = 'Reducing consumption'
 WASTE = 'Material waste'
 HOBBIES = 'Personal Hobbies'
 
 
 PERSONALIZATION = {
     'mapping': {
-        RED_SQUARE: [WILDLIFE],
         COMM_BLDG: [CARBON, WASTE],
         HUB: [COMPOST],
         UW_MG: [COMPOST, GARDENING],
         ROBERTS_HALL: [MICRO, WASTE],
         LIFE_SCI: [CLIMATE],
         HENRY_AG: [ENVIRO, HOBBIES],
-        LAW_LIB: [REDUCING, HOBBIES],
     },
     # 'required': [RED_SQUARE, COMM_BLDG, HUB, HENRY_AG]
 }
 
-LOCATIONS = [RED_SQUARE, COMM_BLDG, HUB,
-             ROBERTS_HALL, UW_MG, LIFE_SCI, HENRY_AG, LAW_LIB]
-
-TOPICS = [WILDLIFE, CARBON, COMPOST, MICRO, GARDENING,
-          CLIMATE, ENVIRO, REDUCING, WASTE, HOBBIES]
+# Only the midpoints, doesn't include red square or law lib
+LOCATIONS = [COMM_BLDG, HUB, ROBERTS_HALL, UW_MG, LIFE_SCI, HENRY_AG]
+TOPICS = [CARBON, COMPOST, MICRO, GARDENING, CLIMATE, ENVIRO, WASTE, HOBBIES]
--- server/services/personalization_service.py
+++ server/services/personalization_service.py
@@ -1,19 +1,28 @@
-from constants import PERSONALIZATION, LOCATIONS, TOPICS
+import numpy as np
+
+from constants import HENRY_AG, PERSONALIZATION, LOCATIONS, TOPICS
+
 
 def get_locs(options):
-
-    return [
-        bool(len(
+    matches = [
+        loc for loc in LOCATIONS
+        if len(
             set([TOPICS[i] for i in range(len(TOPICS)) if options[i]])
-            .intersection(set(PERSONALIZATION['mapping'][loc]))
-        )) for loc in LOCATIONS
+            .intersection(set(PERSONALIZATION['mapping'][loc])))
     ]
 
+    if len(matches) >= 2:
+        midpoints = np.random.choice(matches, 2, replace=False)
+    elif len(matches) == 1:
+        midpoints = np.random.choice(
+            matches + [np.random.choice(list(set(LOCATIONS) - set(matches)))],
+            2, replace=False
+        )
+    else:
+        midpoints = np.random.choice(LOCATIONS, 2, replace=False)
 
-def test():
-    opt = [False, True, True, False, True, True, False, True, True, False]
-    print(get_locs(opt))
+    if midpoints[0] == HENRY_AG:
+        midpoints = reversed(midpoints)
 
-
-if __name__ == '__main__':
-    test()
+    midpoints = [LOCATIONS.index(e) + 1 for e in midpoints]    
+    return [0] + midpoints + [7]
