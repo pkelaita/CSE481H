2a28e2f743763428c2cbfe9250f7a043e1e0d202
pkelaita <pierce@kelaita.com>
2021-06-07 00:43:02 -0700

    fixed

--- server/routes/submit.py
+++ server/routes/submit.py
@@ -6,14 +6,14 @@ submit_bp = Blueprint('submit_bp', __name__)
 endpoint = 'submit'
 
 correct = {
+    0: 2,
     1: 2,
-    2: 2,
-    3: 1,
-    4: 2,
-    5: 3,
-    6: 1,
-    7: 2,
-    8: 1
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 1,
+    6: 2,
+    7: 1
 }
 
 
